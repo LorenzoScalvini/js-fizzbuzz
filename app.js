46194fbcc3fdc259cfa1c29aa820853227be7f8f
LorenzoScalvini <lorenzo.scalvini1704@gmail.com>
2024-10-15 15:10:20 +0200

    numeri da  1 a cento

---
+++ app.js
@@ -0,0 +1,12 @@
+/* Scrivi un programma che stampi i numeri da 1 a 100,
+ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz. 
+Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz. */
+
+// stampare numeri uno a cento
+for (let i = 0; i < 100; i++ ){
+    num = i + 1
+    console.log(num)
+}
+// fizz multipli di 3
+//per i multipli di 5 buzz 
+//per i multipli di 5 e 3 stampi fizz buzz
